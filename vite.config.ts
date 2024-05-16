0bed0ee1d51590a8607ae59e2551813dded04cc4
Tai <tantai.it95@gmail.com>
2024-05-16 23:31:13 +0700

    Revert "Revert "fix: load env""
    This reverts commit c1834b8ccd38a6373d9919932b798fd7be9e34f6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // vite config
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
